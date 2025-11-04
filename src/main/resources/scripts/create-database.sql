564a127708f8fba6d1fa36d3f356532cb9bcd83e
vitor <vitorvieira2200@gmail.com>
2025-11-03 22:14:41 -0300

    feat: ajustes no docker

--- src/main/resources/scripts/create-database.sql
+++ src/main/resources/scripts/create-database.sql
@@ -1,21 +1,11 @@
 -- Script para criar o banco de dados PostgreSQL
 -- Execute este script como superusuário do PostgreSQL antes de iniciar a aplicação
 
--- Criar o banco de dados
-CREATE DATABASE oficina_db
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'Portuguese_Brazil.1252'
-    LC_CTYPE = 'Portuguese_Brazil.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
-
--- Conectar ao banco de dados
-\c oficina_db
+-- Nota: O banco de dados 'tech_challenge' já é criado automaticamente pelo docker-compose
+-- através da variável POSTGRES_DB. Este script pode conter configurações adicionais se necessário.
 
 -- Comentário sobre o banco de dados
-COMMENT ON DATABASE oficina_db IS 'Banco de dados do sistema de gestão de oficina mecânica - Tech Challenge';
+COMMENT ON DATABASE tech_challenge IS 'Banco de dados do sistema de gestão de oficina mecânica - Tech Challenge';
 
 -- As tabelas serão criadas automaticamente pelo Hibernate com base nas entidades JPA
 -- quando a aplicação for iniciada pela primeira vez (spring.jpa.hibernate.ddl-auto=update)
